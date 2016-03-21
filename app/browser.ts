ccecbac4781472822e8f0d6428a0eb64eb70f790
Anna Lawley and Joseph Karasek <alawley1@gmail.com>
2016-03-21 11:51:43 -0700

    add html new entry form

--- app/browser.ts
+++ app/browser.ts
@@ -1,6 +1,5 @@
 /// <reference path="journal-interfaces.ts" />
 
 $(document).ready(function () {
-  var testEntry = new Journal.Entry('Anna', 'I love Typescript', 'Wow, Typescript is so awesome.');
-  console.log(testEntry);
+
 });
