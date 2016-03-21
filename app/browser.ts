f75a012448f3a545b3eb3814e142b6393ce8ab74
Anna Lawley and Joseph Karasek <joseph.karasek@gmail.com>
2016-03-21 11:58:13 -0700

    add jquery for entry input form

--- app/browser.ts
+++ app/browser.ts
@@ -1,5 +1,16 @@
 /// <reference path="journal-interfaces.ts" />
 
 $(document).ready(function () {
-
+  var entries = [];
+  $('#entrysubmit').click(function() {
+    var author : string = $('#author').val();
+    $('#author').val('');
+    var title : string = $('#title').val();
+    $('#title').val('');
+    var text : string = $('#text').val();
+    $('#text').val('');
+    var newEntry = new Journal.Entry(author, title, text);
+    entries.push(newEntry);
+    console.log(entries);
+  });
 });
